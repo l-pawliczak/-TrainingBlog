594a703c3f01e3c977c0b87d7565cbb043eb22a2
l-pawliczak <pawliczaklukasz@gmail.com>
2016-11-20 22:42:56 +0000

    update entities

--- src/BlogBundle/Entity/AbstractTaxonomy.php
+++ src/BlogBundle/Entity/AbstractTaxonomy.php
@@ -36,4 +36,60 @@ abstract class AbstractTaxonomy
     private $slug;
 
     protected $posts;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return AbstractTaxonomy
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return AbstractTaxonomy
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
 }
--- src/BlogBundle/Entity/Tag.php
+++ src/BlogBundle/Entity/Tag.php
@@ -22,4 +22,44 @@ class Tag extends AbstractTaxonomy{
     protected $posts;
     
     
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->posts = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add posts
+     *
+     * @param \BlogBundle\Entity\Post $posts
+     * @return Tag
+     */
+    public function addPost(\BlogBundle\Entity\Post $posts)
+    {
+        $this->posts[] = $posts;
+
+        return $this;
+    }
+
+    /**
+     * Remove posts
+     *
+     * @param \BlogBundle\Entity\Post $posts
+     */
+    public function removePost(\BlogBundle\Entity\Post $posts)
+    {
+        $this->posts->removeElement($posts);
+    }
+
+    /**
+     * Get posts
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getPosts()
+    {
+        return $this->posts;
+    }
 }
